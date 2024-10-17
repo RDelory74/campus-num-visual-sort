e8331d1ed5e26e22e4eaca2f4838f50d033115d7
RDelory74 <roro_spike@hotmail.com>
2024-10-17 11:03:02 +0200

    Done HeapSort.java

--- src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
@@ -9,32 +9,54 @@ import edu.campusnum.visualsort.model.ObservableArray;
  * Time: 16:31
  */
 public class HeapSort implements SortAlgorithm{
+
+
     @Override
     public void sort(ObservableArray array) {
         try {
             int longueur = array.getLength();
             organiser(array);
-            for (int i = longueur -1; i >= 0; i--) {
-                array.swap(i, longueur);
+            for (int i = longueur -1; i > 0; i--) {
+                array.swap(0, i);
+                redescendre(array,i,0);
             }
-
         }
         catch (Exception e) {
             throw new UnsupportedOperationException("Not implemented");
-        }}
+        }
+    }
+
+
+
+    //Méthodes
     public void organiser (ObservableArray array){
         for (int i = 0; i < array.getLength()-1; i++) {
             remonter(array,i);
         }
     }
+
     public void remonter (ObservableArray array, int i) {
-        if (i > i/2){
+        if (array.get(i) > array.get(i/2)){
             array.swap(i,i/2);
             remonter(array,i/2);
         }
     }
-    public void redescendre (ObservableArray array, int element,  int i) {
 
+    public void redescendre (ObservableArray array,int element,int i) {
 
+        int formule = (2* i) +1;
+        int max;
+
+        if (formule < element){
+            if (array.get(formule)>array.get(2*i)) {
+               max = formule;
+            } else {
+                max = 2*i;
+            }
+            if (array.get(max)> array.get(i)){
+                array.swap(max,i);
+                redescendre(array,element,max);
+            }
+        }
     }
 }
